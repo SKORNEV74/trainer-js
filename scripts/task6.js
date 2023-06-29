1068b0c5ffa36a60c06d02b17508f9837eceec9a
SKORNEV74 <yasher7495@yandex.ru>
2023-06-30 02:15:07 +0300

    task 8

--- scripts/task6.js
+++ scripts/task6.js
@@ -2,7 +2,7 @@ let legA = +prompt("leg A:");
 let legB = +prompt("leg B:");
 let hypotenuseC = +prompt("hypotenuse C:");
 
-let condition = Math.pow(legA, 2) + Math.pow(legB, 2) === Math.pow(hypotenuseC, 2)
+let condition = Math.pow(legA, 2) + Math.pow(legB, 2) === Math.pow(hypotenuseC, 2);
 
 let answer = (condition) ? "Это прямоугольный треугольник!" : "Это не прямоугольный треугольник!";
 
---
+++ scripts/task8.js
@@ -0,0 +1,7 @@
+let N = +prompt("number N:");
+
+let answer;
+
+answer = (N % 2 === 0) ? `Число ${N} четное!` : `Число ${N} нечетное!`;
+
+alert(answer);
