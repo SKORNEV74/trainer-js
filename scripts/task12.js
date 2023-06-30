2d8766cd4759ca11354af0f5f0cdea32fd73b7f1
SKORNEV74 <yasher7495@yandex.ru>
2023-07-01 00:49:59 +0300

    tasks 12, 13

---
+++ scripts/task12.js
@@ -0,0 +1,7 @@
+function countDown (n) {
+    for (let i = n; i > 0; i--) {
+        console.log(i);
+    }
+}
+
+countDown(5);
---
+++ scripts/task13.js
@@ -0,0 +1,5 @@
+function getPercents (percent, number) {
+    return percent * number * 0.01;
+}
+
+console.log(getPercents(30, 200));
